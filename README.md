3bb0b6c0312269548def45555f39482ae7f4ee59
saisundarv <56323885+saisundarv@users.noreply.github.com>
2020-05-27 17:03:15 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Project 1
 
 Web Programming with Python and JavaScript
+
+1) you can register email id and password in sign up.
+2) password is encrypted and stored in database
+
+3) you can login in Login page by entering email id and password. Password will be decrypted and compared with database.
+after successful login , it allow you to search the book with keyword like title, author, isbn
+4) if matching record exist it will list the details using LINK on ISBN  else it will through error and it will show button to bring back to search book.
+5) Click on the ISBN to get the review from website and good read review
+6) you can submi the review with a rating and comment
+7) after successfully submitted the review, it will show button to bring back to the search book.
+8) Logout button will clear the session.
+9) going back after the logout session will take you to the login screen.
--- application.py
+++ application.py
@@ -34,6 +34,7 @@ def index():
 
 @app.route("/register", methods=["POST","GET"])
 def register():
+    
     # Get form information.
     if request.method == "POST":
         emailid = request.form.get("emailid")
@@ -57,6 +58,8 @@ def login():
             return render_template("error.html", message="User Name and password doesn't exist.")
                 
         if db.execute("SELECT * FROM USERS WHERE EMAILID = :EMAILID AND password = crypt(:password, password)", {"EMAILID": emailid,"password": password}).rowcount == 1:
+            session['logged_in'] = True
+			
             return render_template("BookReview.html", message='Logged in as: ' + session['emailid'] + " successfully" )        
             
     if request.method == "GET":
@@ -74,71 +77,76 @@ def bookreview():
 @app.route("/bookreview_search/<int:book_id>", methods=["POST","GET"])
 def bookreview_search(book_id):
     """List details about a Book."""
-    global review_rating
-    global isbn_no
-    # Make sure Book exists.
-    book = db.execute("SELECT * FROM books WHERE id = :id", {"id": book_id}).fetchone()
- 
-    if book is None:
-        return render_template("error.html", message="No such book.")
-
-    # Get all book details.
-    book_details = db.execute("SELECT * FROM books WHERE id = :id",
-        {"id": book_id}).fetchall()
-    # Make sure Book exists.
-
-    
-    if book_details is None:
-        book_details="No Book Exist"
-    
-    #book_review = db.execute("SELECT * FROM REVIEW WHERE book_id = :id", {"id": book_id}).fetchall()
-    if db.execute("SELECT * FROM REVIEW WHERE book_id = :id", {"id": book_id}).rowcount == 0: 
-        review_rating_msg="No Rating Exist"
+    if not session.get('logged_in'):
+        print("Not logged in")
+        return redirect(url_for('login'))
     else:
-        review_rating = db.execute("select book_id,avg(rating) from review where book_id=:id group by book_id",
-            {"id": book_id}).fetchone()
-        review_rating_msg=review_rating.avg
-    loggedinas=session['emailid']
-    
-    user_details=db.execute("SELECT * FROM USERS WHERE EMAILID = :EMAILID", 
-        {"EMAILID": loggedinas}).fetchall()
-    
-    isbn_no= db.execute("SELECT isbn FROM books WHERE id = :id",
-        {"id": book_id}).fetchall()
+        global review_rating
+        global isbn_no
+        # Make sure Book exists.
+        book = db.execute("SELECT * FROM books WHERE id = :id", {"id": book_id}).fetchone()
+     
+        if book is None:
+            return render_template("error.html", message="No such book.")
+
+        # Get all book details.
+        book_details = db.execute("SELECT * FROM books WHERE id = :id",
+            {"id": book_id}).fetchall()
+        # Make sure Book exists.
+
         
-    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "8IscQCdcmYRmqNtnP27kTQ", "isbns": isbn_no})
-    if res.status_code != 200:
-        raise Exception("ERROR: API request unsuccessful.")
-    Goodreads_res=res.json()
- #   Goodreads_res=Goodreads_review['books']['average_rating']
- #   return render_template("SubmitReview.html", Booksdetails=book_details,  review_rating=review_rating_msg, Goodreads_res=Goodreads_res)    
-    return render_template("SubmitReview.html", user_details=user_details, Booksdetails=book_details,  review_rating=review_rating_msg, Goodreads_res=Goodreads_res)
+        if book_details is None:
+            book_details="No Book Exist"
+        
+        #book_review = db.execute("SELECT * FROM REVIEW WHERE book_id = :id", {"id": book_id}).fetchall()
+        if db.execute("SELECT * FROM REVIEW WHERE book_id = :id", {"id": book_id}).rowcount == 0: 
+            review_rating_msg="No Rating Exist"
+        else:
+            review_rating = db.execute("select book_id,avg(rating) from review where book_id=:id group by book_id",
+                {"id": book_id}).fetchone()
+            review_rating_msg=review_rating.avg
+        loggedinas=session['emailid']
+        
+        user_details=db.execute("SELECT * FROM USERS WHERE EMAILID = :EMAILID", 
+            {"EMAILID": loggedinas}).fetchall()
+        
+        isbn_no= db.execute("SELECT isbn FROM books WHERE id = :id",
+            {"id": book_id}).fetchall()
+            
+        res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "8IscQCdcmYRmqNtnP27kTQ", "isbns": isbn_no})
+        if res.status_code != 200:
+            raise Exception("ERROR: API request unsuccessful.")
+        Goodreads_res=res.json()
+     #   Goodreads_res=Goodreads_review['books']['average_rating']
+     #   return render_template("SubmitReview.html", Booksdetails=book_details,  review_rating=review_rating_msg, Goodreads_res=Goodreads_res)    
+        return render_template("SubmitReview.html", user_details=user_details, Booksdetails=book_details,  review_rating=review_rating_msg, Goodreads_res=Goodreads_res)
         
 @app.route("/searchbook", methods=["POST","GET"])
 def searchbook():
-    if request.method == "POST":
-        BookName = request.form.get("BookName")
-        formatBookName = "%{}%".format(BookName)       
-        
-        if db.execute("select * from books where LOWER(isbn) like :BookName OR LOWER(title) like :BookName OR LOWER(author) like :BookName", {"BookName": formatBookName}).rowcount == 0:
-            return render_template("error.html", message="No Book details exist.")
+    if not session.get('logged_in'):
+        print("Not logged in")
+        return redirect(url_for('login'))
+    else:
+        if request.method == "POST":
+            BookName = request.form.get("BookName")
+            formatBookName = "%{}%".format(BookName)       
+            
+            if db.execute("select * from books where LOWER(isbn) like :BookName OR LOWER(title) like :BookName OR LOWER(author) like :BookName", {"BookName": formatBookName}).rowcount == 0:
+                return render_template("BacktoSearch.html", message="No Book details exist.")
 
-        Books_found=db.execute("select * from books where LOWER(isbn) like :BookName OR LOWER(title) like :BookName OR LOWER(author) like :BookName", {"BookName": formatBookName}).fetchall()
-        return render_template("BookDetails.html", Booksdetails=Books_found)        
-                
-    if request.method == "GET":
-        if (len(session['name'])  == 0):
-            print ("Yes") 
-            return render_template("BookReview.html",message='Logged in as: ' + session['name'] )
-        else :  
-            print ("No") 
-            return render_template("error.html", message="Invalid Session.")
+            Books_found=db.execute("select * from books where LOWER(isbn) like :BookName OR LOWER(title) like :BookName OR LOWER(author) like :BookName", {"BookName": formatBookName}).fetchall()
+            return render_template("BookDetails.html", Booksdetails=Books_found)        
+                    
+        if request.method == "GET":
+            return render_template("BookReview.html",message='Logged in as: ' + session['emailid'] )
 
 @app.route("/logout")
 def logout():
     loggedoutfrom=session['emailid']
     session.pop('emailid', None)
-    session.clear()
+    session['logged_in'] = False
+    session.clear()  
+    ### Added to clear session. 
     return render_template("index.html", session='Logged out as User: ' + loggedoutfrom )
 
 @app.after_request
@@ -149,19 +157,23 @@ def after_request(response):
 
 @app.route("/submit_review", methods=["POST","GET"])
 def submit_review():
-    if request.method == "GET":
-        return render_template("success.html", message="Inside Submt_review !!")
-    if request.method == "POST":
-        rating_points = request.form.get("rating_points")
-        review_comments = request.form.get("review_comments")
-        user_id = request.form.get("user_id")
-        user_name = request.form.get("user_name")        
-        book_id = request.form.get("book_id")
-        book_isbn = request.form.get("book_isbn") 
-        db.execute("INSERT INTO review (book_id , book_isbn , user_id , user_name , rating , review_comments , created_at  ) VALUES (:book_id, :book_isbn, :user_id,:user_name,:rating , :review_comments,:curr_time)",
-            {"book_id": book_id, "book_isbn": book_isbn, "user_id": user_id, "user_name": user_name, "rating": rating_points,"review_comments": review_comments, "curr_time": datetime.datetime.utcnow()})
-        db.commit()
-        return render_template("success.html", message="You have successfully Registered the Review.")
+    if not session.get('logged_in'):
+        print("Not logged in")
+        return redirect(url_for('login'))
+    else:
+        if request.method == "GET":
+            return render_template("success.html", message="Inside Submt_review !!")
+        if request.method == "POST":
+            rating_points = request.form.get("rating_points")
+            review_comments = request.form.get("review_comments")
+            user_id = request.form.get("user_id")
+            user_name = request.form.get("user_name")        
+            book_id = request.form.get("book_id")
+            book_isbn = request.form.get("book_isbn") 
+            db.execute("INSERT INTO review (book_id , book_isbn , user_id , user_name , rating , review_comments , created_at  ) VALUES (:book_id, :book_isbn, :user_id,:user_name,:rating , :review_comments,:curr_time)",
+                {"book_id": book_id, "book_isbn": book_isbn, "user_id": user_id, "user_name": user_name, "rating": rating_points,"review_comments": review_comments, "curr_time": datetime.datetime.utcnow()})
+            db.commit()
+            return render_template("BacktoSearch.html", message="You have successfully Registered the Review.")
 
 
 
